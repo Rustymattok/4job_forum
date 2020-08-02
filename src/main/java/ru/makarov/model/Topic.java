596a6c2fc343f35bf329c19d54476518496be2a8
Rustymattok <mrArxi@gmail.com>
2020-08-02 14:49:08 +0300

    make it better

--- src/main/java/ru/makarov/model/Topic.java
+++ src/main/java/ru/makarov/model/Topic.java
@@ -28,7 +28,7 @@ public class Topic {
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     private User author;
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "topic_comments", joinColumns = @JoinColumn(name = "topic_id"), inverseJoinColumns = @JoinColumn(name = "comments_id"))
-    private Set<Comments> comments;
+//    @ManyToMany(fetch = FetchType.EAGER)
+//    @JoinTable(name = "topic_comments", joinColumns = @JoinColumn(name = "topic_id"), inverseJoinColumns = @JoinColumn(name = "comments_id"))
+//    private Set<Comments> comments;
 }
--- src/main/resources/db/insert.sql
+++ src/main/resources/db/insert.sql
@@ -1,18 +1,4 @@
 insert into usr values (1,TRUE,'mrArxi@gmail.com','Vladimir','Vladimir','12345','Rustymattok');
 insert into user_role values (1,'ADMIN');
-CREATE TABLE topic_comments
-(
-  topic_id bigint NOT NULL,
-  comments_id bigint NOT NULL,
-  CONSTRAINT topic_comments_pkey PRIMARY KEY (topic_id, comments_id),
-    CONSTRAINT fkia810jj3eu2p5ntkhvjp5y9t0 FOREIGN KEY (topic_id)
-      REFERENCES topic (id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION,
-  CONSTRAINT fkllpw88mcv1vyajo7oiyol5ys4 FOREIGN KEY (comments_id)
-      REFERENCES comments (id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION
-);
 insert into topic values (1,'smth','2016-06-22 19:10:25-07','Vladimir',1);
 insert into comments values (1,'spring boot help',1,1);
--- Heroku = сука
--- Heroku = сука дважды
